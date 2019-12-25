aad155b0a2b0f5ecfc9f653dd165ab335f6cd246
David Isakson <ikeman32@yahoo.com>
2019-12-24 22:12:56 -0800

    Chaged first push to include pushing to

--- gitscripts/first-push.sh
+++ gitscripts/first-push.sh
@@ -1,5 +1,16 @@
 #!/bin/bash 
 
-git add .&&  
-git commit -m "initial commit"&&  
-git push -u origin master
+read -p "Push to master y/n" ans&&
+if [ans="y"]
+then
+    git add .&&  
+    git commit -m "initial commit"&&  
+    git push -u origin master
+    exit 0
+else
+    read -p "New branch " branch&&
+    git add .&&  
+    git commit -m "initial commit"&&  
+    git push -u origin $branch
+    exit 0
+fi
