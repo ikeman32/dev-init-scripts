7919a3b79dbbc232b4186b15413e16967347e853
David Isakson <ikeman32@yahoo.com>
2019-12-24 21:56:36 -0800

    edit push

--- push.sh
+++ push.sh
@@ -1,13 +1,11 @@
 #!/bin/bash 
 
-read -p 'Enter commit message: ' msg 
-
-echo
-
 git add .&&
 
-git commit -m $msg&&
+read -p 'Enter commit message: ' msg &&
 
-clear
+git commit -m "$msg"&&
+
+clear&&
 
 git push
